1eef2f65b291778e0ba78eb635d94b5dce797168
tberkovac <tberkovac1@etf.unsa.ba>
2022-06-05 00:42:10 +0200

    Dodan DelivererOrders u dbcontext

--- SavoirApp/Data/ApplicationDbContext.cs
+++ SavoirApp/Data/ApplicationDbContext.cs
@@ -17,6 +17,7 @@ namespace SavoirApp.Data
         public DbSet<ItemSizes> ItemSizes { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItems> OrderItems { get; set; }
+        public DbSet<DelivererOrders> DelivererOrders { get; set; }
         public DbSet<RegisteredUserOrders> RegisteredUserOrders { get; set; }
         public DbSet<User> VIPUsers { get; set; }
         public DbSet<VIPUserOrders> VIPUserOrders { get; set; }
@@ -29,6 +30,7 @@ namespace SavoirApp.Data
             modelBuilder.Entity<ItemSizes>().ToTable("ItemSizes");
             modelBuilder.Entity<Order>().ToTable("Orders");
             modelBuilder.Entity<OrderItems>().ToTable("OrderItems");
+            modelBuilder.Entity<DelivererOrders>().ToTable("DelivererOrders");
             modelBuilder.Entity<RegisteredUserOrders>().ToTable("RegisteredUserOrders");
             modelBuilder.Entity<User>().ToTable("VIPUsers");
             modelBuilder.Entity<VIPUserOrders>().ToTable("VIPUserOrders");
