94d4d2a1b059b55a899e67f44112f540fb79228c
DervisevicE <edervisevi2@etf.unsa.ba>
2022-06-08 15:55:37 +0200

    HomeScreen

--- SavoirApp/Data/Migrations/20220530174608_PrvaMigracija.Designer.cs
+++ SavoirApp/Data/Migrations/20220530174608_PrvaMigracija.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SavoirApp.Data;
 
 namespace SavoirApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220530174608_PrvaMigracija")]
+    partial class PrvaMigracija
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SavoirApp/Data/Migrations/20220530174608_PrvaMigracija.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SavoirApp.Data.Migrations
+{
+    public partial class PrvaMigracija : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
